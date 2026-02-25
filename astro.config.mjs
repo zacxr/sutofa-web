bea180bf7204ed17618372916a9eb530e8ae993c
Zacxr <zacharyxr9@gmail.com>
2026-02-25 22:36:15 +0800

    fix cloudflare deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import cloudflare from '@astrojs/cloudflare'; // 确保引入了它
 
 export default defineConfig({
-server: {
-    allowedHosts: ['.trycloudflare.com']
-},
-site: "https://astro-nano-demo.vercel.app",
-integrations: [mdx(), sitemap(), tailwind()],
+  output: 'server', // 或者 'hybrid'，取决于你是否需要 SSR
+  adapter: cloudflare(),
 });
